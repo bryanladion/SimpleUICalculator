a463519015434eab819065f8294993e9a4f786d2
bryanladion <ladionbryan29@gmail.com>
2021-10-08 22:02:38 +0800

    I'm struggling I need to come up with another plan a this time.

--- app/src/main/java/com/newproject/simpleuicalculator/MainActivity.kt
+++ app/src/main/java/com/newproject/simpleuicalculator/MainActivity.kt
@@ -6,6 +6,18 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main
+
+        // for making the status bar transparent
+            if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+                setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
+            }
+            if (Build.VERSION.SDK_INT >= 19) {
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            }
+            if (Build.VERSION.SDK_INT >= 21) {
+                setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+                window.statusBarColor = Color.TRANSPARENT
+            }
     }
 }
